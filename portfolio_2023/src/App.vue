afc2e17494ec04df6ff14ed8b173492a49cf0a3b
Dounya Derlen <dounyaderlen@MacBook-Air-de-Dounya.local>
2023-02-17 14:59:56 +0100

    first push

---
+++ portfolio_2023/src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import Navbar from "../src/components/Navbar.vue"
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      <nav>
+        <Navbar />
+        <!-- <RouterLink to="/">Home</RouterLink> -->
+      </nav>
+    </div>
+  </header>
+  <RouterView />
+</template>
---
+++ portfolio_2023/src/components/Navbar.vue
@@ -0,0 +1,20 @@
+<script setup></script>
+<template>
+  <div>
+    <nav class="navbar m-1 font-15">
+			<div class="d-flex">
+				<img src="" class="logo-class mr-1" />
+				<!-- <p class="my-auto">Dounya Derlen</p> -->
+			</div>
+
+			<div class="d-flex align-items-center cursor menu-container">
+				<a class="menu-item mr-2 letter-space-1">Mes projets webs</a>
+				<a class="menu-item letter-space-1">Mes conceptions UI</a>
+			</div>
+
+			<div class="d-flex">
+				<button id="contact-btn">Me contacter</button>
+			</div>
+    </nav>
+  </div>
+</template>
