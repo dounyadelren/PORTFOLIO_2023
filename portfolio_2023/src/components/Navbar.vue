ebc81418d951c9487b7580481311009e63e2c799
Dounya Derlen <dounyaderlen@MacBook-Air-de-Dounya.local>
2023-02-23 15:09:15 +0100

    update - navbar dynamique

--- portfolio_2023/src/components/Navbar.vue
+++ portfolio_2023/src/components/Navbar.vue
@@ -1,23 +1,69 @@
 <script setup>
+import { useRoute } from "vue-router";
+import { onMounted } from "vue";
+import { watch, ref } from "vue";
+import router from "../router";
+
+const route = useRoute();
+const navbarUn = ref(true);
+const navbarDeux = ref(false);
+let type = ref("");
+onMounted(() => {
+  type.value = route.params.type;
+});
+
+watch(
+  () => route.path,
+  () => {
+    console.log(route.path);
+    if (route.path == "/projects/web" || route.path == "/projects/ui") {
+      navbarDeux.value = true;
+      navbarUn.value = false;
+    } else {
+      navbarDeux.value = false;
+      navbarUn.value = true;
+    }
+  }
+);
+
+const redirect = (route) => {
+  router.push("/projects/" + route);
+};
 </script>
 <template>
   <div>
     <nav class="navbar font-12">
       <div class="">
-        <a href="/" ><img src="../assets/img/LogoDD.png" class="logo-class" /></a>
+        <a href="/"
+          ><img src="../assets/img/LogoDD.png" class="logo-class"
+        /></a>
       </div>
-
-      <div class="d-flex align-items-center cursor menu-container justify-content-around">
-        <a href="#web" v-smooth-scroll class="menu-item me-2 letter-space-1">Mes projets webs</a>
-        <a href="#ui" v-smooth-scroll class="menu-item me-2 letter-space-1">Mes conceptions UI</a>
-        <!-- <a href="#cv" v-smooth-scroll class="menu-item letter-space-1">Mon parcours</a> -->
+      <div
+        v-if="navbarUn"
+        class="d-flex align-items-center cursor menu-container justify-content-around"
+      >
+        <a href="#web" v-smooth-scroll class="menu-item me-2 letter-space-1"
+          >Mes projets webs</a
+        >
+        <a href="#ui" v-smooth-scroll class="menu-item me-2 letter-space-1"
+          >Mes conceptions UI</a
+        >
       </div>
-
+      <div
+        v-if="navbarDeux"
+        class="d-flex align-items-center cursor menu-container justify-content-around"
+      >
+        <p @click="redirect('web')" class="menu-item me-2 letter-space-1">
+          Mes projets webs
+        </p>
+        <p @click="redirect('ui')" class="menu-item me-2 letter-space-1">
+          Mes conceptions UI
+        </p>
+      </div>
       <div class="d-flex align-items-center">
         <button id="contact-btn">Me contacter</button>
       </div>
     </nav>
   </div>
 </template>
-<style>
-</style>
+<style></style>
--- portfolio_2023/src/views/ProjectsView.vue
+++ portfolio_2023/src/views/ProjectsView.vue
@@ -5,13 +5,24 @@ import { ref, onMounted } from "vue";
 let type = ref("");
 const route = useRoute();
 
+const scrollToTop = () => {
+  window.scrollTo(0,0);
+}
+
 onMounted(() => {
+  scrollToTop()
   type.value = route.params.type;
+  console.log('type', type.value)
 });
 </script>
 <template>
-  <div class="row">
-    <div class="col-12"><p>test</p></div>
-  </div>
+  <main>
+    <div class="container mx-auto">
+      <div class="row">
+        <div class="col-4 offset-md-2" style="max-height: 359px">test</div>
+        <div class="col-4" style="max-height: 359px">test</div>
+      </div>
+    </div>
+  </main>
 </template>
 <style scoped></style>
